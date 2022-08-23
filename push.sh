0a50d2ac6e734ad8f58c73a5706ab5a866781a5e
server <ubuntu@ip-172-31-44-26.eu-west-3.compute.internal>
2022-08-23 10:54:35 +0000

    20220823-10:54:35

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 
 # Hay que tener en cuenta que crontab utiliza el la shell SH, a diferencia de los entornos de escritorio que suelen utilizar BASH.
@@ -8,16 +8,14 @@ echo $PATH
 echo $1
 
 
-function upload(){
-	git add .
+git add .
+if [[ $? == 0 ]]; then
+	date '+\%Y\%m\%d-\%H:\%M:\%S' | xargs git commit -m
 	if [[ $? == 0 ]]; then
-		date '+%Y%m%d-%H:%M:%S' | xargs git commit -m
-		if [[ $? == 0 ]]; then
-			echo "push"
-			git push
-			date +%D >> done.txt
-		fi
+		echo "push"
+		git push
+		date +%D >> done.txt
 	fi
-}
+fi
 
-upload
+
