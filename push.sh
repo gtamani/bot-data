739a2cf63b8a06642a70ad19fa01c4a12b89f444
server <ubuntu@ip-172-31-44-26.eu-west-3.compute.internal>
2022-08-23 11:06:17 +0000

    20220823-11:06:17

--- push.sh
+++ push.sh
@@ -4,11 +4,12 @@
 # Hay que tener en cuenta que crontab utiliza el la shell SH, a diferencia de los entornos de escritorio que suelen utilizar BASH.
 # En SH, el simbolo % tiene que escaparse
 export PATH="/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-echo $PATH >> logs.txt
-echo $1
+echo $PATH > logs.txt
+echo $1 >> logs.txt
 
 
 git add .
+echo 'added' >> logs.txt
 if [[ $? == 0 ]]; then
 	date '+\%Y\%m\%d-\%H:\%M:\%S' | xargs git commit -m
 	if [[ $? == 0 ]]; then
